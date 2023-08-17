6f05e4066b3b5fef3c82264844f146a4f3880c40
gagansankhla <sankhlaga@gmail.com>
2023-08-17 15:15:42 +0530

    Add filter field and card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Filter from './components/Filter';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Divider } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Cards from './components/Cards';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    <Navbar />
+    <div
+      style={{
+        paddingLeft: '4rem',  
+        paddingRight: '4rem', 
+        backgroundColor: '#0D0D0D', 
+        minHeight: '100vh', 
+        color: 'white', 
+      }}
+    >
+      
+      <Typography 
+        variant="h2" 
+        padding={5} 
+        textAlign={'start'}
+        fontFamily="sans-serif"
+        fontWeight={500}
+        fontSize="5.5vw" 
+      >
+        Sirius
+      </Typography>
+
+      <Divider 
+        variant="middle"
+        sx={{
+          width: '100%',
+          height: '1px',
+          backgroundColor: '#262728',
+          margin: '0 auto',
+          marginBottom: '2rem',
+        }}
+      />
+      <Typography 
+        variant="h4"
+        fontSize="30px"
+        fontFamily="sans-serif"
+        fontWeight={700}
+        padding={2}
+      >
+      Filter results</Typography>
+      <Grid container spacing={2} padding={1}>
+        <Grid item xs={12} sm={6} md={4} lg={2.5}>
+          <Filter />
+          </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={2.5}>
+          <Filter />
+          </Grid>
+        </Grid>
+        <div>
+        <Cards />
+        </div>
     </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,130 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+const pages = ["Stars", "Planets", "Astronomy", "Notification"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar
+      sx={{ backgroundColor: "#383B3E", height: "100px", paddingLeft: '5rem', paddingRight: '5rem',}}
+      position="static"
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{ top: "20%" }}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              fontFamily: "sans-serif",
+              fontWeight: 700,
+              fontSize: "3rem",
+              color: "inherit",
+              textDecoration: "none",  
+            }}
+          >
+            Search
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "", md: "none" }}}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+              
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex", justifyContent: "end" },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  fontSize: "1.2rem",
+                  fontWeight: "400",
+                  textTransform: "initial",
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+            <IconButton size="large" aria-label="search" color="inherit">
+              <SearchIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
